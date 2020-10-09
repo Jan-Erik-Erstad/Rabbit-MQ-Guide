738245ca5d5b92ed1c07f2e89bb5d95e227f76e3
Jan-Erik-Erstad <jan.erik.erstad@gmail.com>
2020-10-09 13:17:34 +0200

    publish/subscribe guide followed

---
+++ src/main/java/com/rabbitmq/publishsubscribe/EmitLog.java
@@ -0,0 +1,21 @@
+package com.rabbitmq.publishsubscribe;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            String message = String.join(" ", argv);
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
