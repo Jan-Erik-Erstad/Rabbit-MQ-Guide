7216fb44a2e598a18a1ce77db94747df40ab54cf
Jan-Erik-Erstad <jan.erik.erstad@gmail.com>
2020-10-09 11:53:07 +0200

    Marking the massages as durable This will ensure that the messages wont be lost even if the connection to RabbitMQ is lost

--- src/main/java/com/rabbitmq/workqueue/NewTask.java
+++ src/main/java/com/rabbitmq/workqueue/NewTask.java
@@ -3,19 +3,22 @@ package com.rabbitmq.workqueue;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import java.nio.charset.StandardCharsets;
+import com.rabbitmq.client.MessageProperties;
 
 public class NewTask {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "task_queue";
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            boolean durable = true;
+            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
             String message = String.join(" ", argv);
 
-            channel.basicPublish("", "hello", null, message.getBytes());
+            channel.basicPublish("", QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
--- src/main/java/com/rabbitmq/workqueue/Worker.java
+++ src/main/java/com/rabbitmq/workqueue/Worker.java
@@ -6,15 +6,18 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Worker {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "task_queue";
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        boolean durable = true;
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1); // accept only one unack-ed message at a time (see below)
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
@@ -26,9 +29,10 @@ public class Worker {
                 e.printStackTrace();
             } finally {
                 System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-        boolean autoAck = true; // acknowledgment is covered below
+        boolean autoAck = false;
         channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
     }
     private static void doWork(String task) throws InterruptedException {
