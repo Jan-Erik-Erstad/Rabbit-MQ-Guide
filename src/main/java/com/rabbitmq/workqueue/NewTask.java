db6cf5d0cac351e41cc1562b2000c6eed95399be
Jan-Erik-Erstad <jan.erik.erstad@gmail.com>
2020-10-09 12:10:17 +0200

    Making the queue fair

--- src/main/java/com/rabbitmq/workqueue/NewTask.java
+++ src/main/java/com/rabbitmq/workqueue/NewTask.java
@@ -6,17 +6,16 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
 public class NewTask {
-    private final static String QUEUE_NAME = "task_queue";
+    private static final String TASK_QUEUE_NAME = "task_queue";
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            boolean durable = true;
-            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
             String message = String.join(" ", argv);
 
-            channel.basicPublish("", QUEUE_NAME,
+            channel.basicPublish("", TASK_QUEUE_NAME,
                     MessageProperties.PERSISTENT_TEXT_PLAIN,
                     message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
--- src/main/java/com/rabbitmq/workqueue/Worker.java
+++ src/main/java/com/rabbitmq/workqueue/Worker.java
@@ -6,22 +6,19 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Worker {
-    private final static String QUEUE_NAME = "task_queue";
+    private static final String QUEUE_NAME = "task_queue";
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        boolean durable = true;
-        channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        channel.basicQos(1); // accept only one unack-ed message at a time (see below)
-
+        channel.basicQos(1);
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-
             System.out.println(" [x] Received '" + message + "'");
             try {
                 doWork(message);
@@ -32,11 +29,11 @@ public class Worker {
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-        boolean autoAck = false;
-        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
+
     private static void doWork(String task) throws InterruptedException {
-        for (char ch: task.toCharArray()) {
+        for (char ch : task.toCharArray()) {
             if (ch == '.') Thread.sleep(1000);
         }
     }
